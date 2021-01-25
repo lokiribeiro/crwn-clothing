49f75047c0400c3395a740ea0a267383800bb078
Loki <loki.hadid@gmail.com>
2021-01-25 14:42:13 +0800

    fixed collection items

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
+import { Route } from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
+import Collection from '../collection/Collection';
 
-import CollectionPreview from '../../components/collection-preview/CollectionPreview';
-
-class Shop extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Shop = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={Collection} />
+    </div>
+  );
+};
 
 export default Shop;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
@@ -13,6 +15,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
