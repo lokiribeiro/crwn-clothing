6318ae683c63cfd61c88ea807289d87e2ec88820
Loki <loki.hadid@gmail.com>
2021-01-27 14:41:58 +0800

    implement code that pulls in our shop data from firestore

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -2,14 +2,68 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 import Collection from '../collection/Collection';
+import { connect } from 'react-redux';
+import WithSpinner from '../../components/with-spinner/WithSpinner';
 
-const Shop = ({ match }) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={Collection} />
-    </div>
-  );
-};
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
-export default Shop;
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(Collection);
+
+class Shop extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    /*fetch(
+      'https://firestore.googleapis.com/v1/projects/crwn-db-9ec29/databases/(default)/documents/collections'
+    )
+      .then((response) => response.json())
+      .then((collections) => console.log(collections));*/
+
+    collectionRef.get().then(async (snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(Shop);
